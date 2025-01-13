6a1b70a49fef56e90e27cfa05d6edaa5b1978e55
Aman Upadhyay <aupadhyay@cleverwizard.com>
2025-01-13 22:58:04 +0530

    Testing git push

--- app/api/test-api/route.ts
+++ app/api/test-api/route.ts
@@ -3,4 +3,5 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function GET(req: NextRequest) {
     console.log('Request received:', req);
     return NextResponse.json({ message: 'Webhook cleared successfully' });
+
 }
